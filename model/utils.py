deba414aeff1b74d1d11aa4835b2c68a234a9d05
HaoyueBaiZJU <hbaiaa@cse.ust.hk>
2021-10-09 17:58:14 +0800

    Add files via upload

---
+++ model/utils.py
@@ -0,0 +1,123 @@
+import os
+import shutil
+import time
+import pprint
+import torch
+import numpy as np
+
+def one_hot(indices, depth):
+    encoded_indicies = torch.zeros(indices.size() + torch.Size([depth]))
+    if indices.is_cuda:
+        encoded_indicies = encoded_indicies.cuda()    
+    index = indices.view(indices.size()+torch.Size([1]))
+    encoded_indicies = encoded_indicies.scatter_(1,index,1)
+
+    return encoded_indicies
+
+def set_gpu(x):
+    os.environ['CUDA_VISIBLE_DEVICES'] = x
+    print('using gpu:', x)
+
+
+def ensure_path(path, remove=True):
+    if os.path.exists(path):
+        if remove:
+            if input('{} exists, remove? ([y]/n)'.format(path)) != 'n':
+                shutil.rmtree(path)
+                os.mkdir(path)
+    else:
+        os.mkdir(path)
+
+class Averager():
+
+    def __init__(self):
+        self.n = 0
+        self.v = 0
+
+    def add(self, x):
+        self.v = (self.v * self.n + x) / (self.n + 1)
+        self.n += 1
+
+    def item(self):
+        return self.v
+
+class AverageMeter(object):
+    """Computes and stores the average and current value"""
+
+    def __init__(self):
+        self.reset()
+
+    def reset(self):
+        self.val = 0
+        self.avg = 0
+        self.sum = 0
+        self.count = 0
+
+    def update(self, val, n=1):
+        self.val = val
+        self.sum += val * n
+        self.count += n
+        self.avg = self.sum / self.count
+
+
+
+def count_acc(logits, label):
+    pred = torch.argmax(logits, dim=1)
+    if torch.cuda.is_available():
+        return (pred == label).type(torch.cuda.FloatTensor).mean().item()
+    else:
+        return (pred == label).type(torch.FloatTensor).mean().item()
+
+
+def accuracy(output, target, topk=(1,)):
+    """Computes the precision@k for the specified values of k"""
+    maxk = max(topk)
+    batch_size = target.size(0)
+
+    _, pred = output.topk(maxk, 1, True, True)
+    pred = pred.t()
+    correct = pred.eq(target.view(1, -1).expand_as(pred))
+
+    res = []
+    for k in topk:
+        correct_k = correct[:k].view(-1).float().sum(0)
+        res.append(correct_k.mul_(100.0/batch_size))
+    return res
+
+def euclidean_metric(a, b):
+    n = a.shape[0]
+    m = b.shape[0]
+    a = a.unsqueeze(1).expand(n, m, -1)
+    b = b.unsqueeze(0).expand(n, m, -1)
+    logits = -((a - b)**2).sum(dim=2)
+    return logits
+
+class Timer():
+
+    def __init__(self):
+        self.o = time.time()
+
+    def measure(self, p=1):
+        x = (time.time() - self.o) / p
+        x = int(x)
+        if x >= 3600:
+            return '{:.1f}h'.format(x / 3600)
+        if x >= 60:
+            return '{}m'.format(round(x / 60))
+        return '{}s'.format(x)
+
+_utils_pp = pprint.PrettyPrinter()
+def pprint(x):
+    _utils_pp.pprint(x)
+
+def compute_confidence_interval(data):
+    """
+    Compute 95% confidence interval
+    :param data: An array of mean accuracy (or mAP) across a number of sampled episodes.
+    :return: the 95% confidence interval for this data.
+    """
+    a = 1.0 * np.array(data)
+    m = np.mean(a)
+    std = np.std(a)
+    pm = 1.96 * (std / np.sqrt(len(a)))
+    return m, pm
